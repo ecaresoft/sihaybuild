7e0df6e99eb41d5d8a07997d3435ce0824a0e125
luissifu <luis.e.sifuentes@gmail.com>
2017-10-16 16:14:48 -0500

    Feature[front]: Add model schema

---
+++ front/models.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.db import models
+
+class Repo(models.Model):
+    name = models.CharField(max_length=50)
+
+class Build(models.Model):
+    BUILD_STATUSES = (
+        ('pending', 'pending'),
+        ('running', 'running'),
+        ('stopped', 'stopped'),
+        ('passed', 'passed'),
+        ('errored', 'errored'),
+    )
+
+    repo = models.ForeignKey(Repo, on_delete=models.CASCADE)
+    branch = models.CharField(max_length=50)
+    committer = models.CharField(max_length=50)
+    status = models.CharField(max_length=7, choices=BUILD_STATUSES)
+    log = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Pipeline(models.Model):
+    repo = models.ForeignKey(Repo, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    branch_pattern = models.CharField(max_length=50)
+    commands = models.TextField()
--- simpleci/settings.py
+++ simpleci/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'webhooks.apps.WebhooksConfig',
+    'front.apps.FrontConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
