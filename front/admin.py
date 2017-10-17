c8e70e69ff33b27790f8bb9bcb3f631705c943cf
luissifu <luis.e.sifuentes@gmail.com>
2017-10-17 15:08:18 -0500

    Feature[front]: Configure models for admin

--- front/admin.py
+++ front/admin.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from models import *
 
-# Register your models here.
+admin.site.register(Repo)
+admin.site.register(Pipeline)
--- front/models.py
+++ front/models.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
+@python_2_unicode_compatible
 class Repo(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
+@python_2_unicode_compatible
 class Build(models.Model):
     BUILD_STATUSES = (
         ('pending', 'pending'),
@@ -21,6 +27,10 @@ class Build(models.Model):
     log = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return "%s/%s: %s" % (self.repo.name, self.branch, self.status)
+
+@python_2_unicode_compatible
 class Pipeline(models.Model):
     repo = models.ForeignKey(Repo, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
@@ -29,3 +39,6 @@ class Pipeline(models.Model):
 
     def get_commands(self):
         return self.commands.split('\n')
+
+    def __str__(self):
+        return "%s (%s)" % (self.name, self.repo)
