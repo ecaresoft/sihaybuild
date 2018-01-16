762bcd9a824bd8f21f395367d598721ba8dc89bd
Ben Orozco <benoror@gmail.com>
2018-01-15 18:39:03 -0600

    Chore: Add logging config

--- simpleci/settings.py
+++ simpleci/settings.py
@@ -128,3 +128,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
+
